809dafe0e50986a1effc2302e65070edee19545e
mathur15 <som.mathur1997@gmail.com>
2021-05-23 23:31:11 -0400

    make sure the longest chain is found among all nodes

--- alt_coint.py
+++ alt_coint.py
@@ -80,6 +80,25 @@ class Blockchain:
         #parse address urlparse
         parsed_url = urlparse(new_address)
         self.nodes.add(parsed_url.netloc)
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        
+        #loop over the nodes in the network to find largest chain
+        for nodes in network:
+            response = request.get(f"http://{nodes}/get_chain")
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if(length > max_length) and is_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+             self.chain = longest_chain
+             return True
+        return False
+                
 # Part 2 - Mining our Blockchain
 
 # Creating a Web App
