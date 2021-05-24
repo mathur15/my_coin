5f89da5367984a83cf86e4a7d1382379461b1060
mathur15 <som.mathur1997@gmail.com>
2021-05-23 21:26:11 -0400

    function to add nodes

--- alt_coint.py
+++ alt_coint.py
@@ -24,6 +24,7 @@ class Blockchain:
         self.chain = []
         self.transactions = [] #before added to the block
         self.create_block(proof = 1, previous_hash = '0')
+        self.nodes = set()
 
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
@@ -75,7 +76,10 @@ class Blockchain:
             'amount':amount})
         previous_block = self.get_previous_block()
         return previous_block['index'] +1
-        
+    def add_nodes(self,new_address):
+        #parse address urlparse
+        parsed_url = urlparse(new_address)
+        self.nodes.add(parsed_url.netloc)
 # Part 2 - Mining our Blockchain
 
 # Creating a Web App
